6c5c4ae9236f5b02ca34629379cf365067035ab7
Jeanlee <jeanlee.barreto@utec.edu.pe>
2021-10-26 11:13:32 -0500

    Add featured cake slider

---
+++ script/app.js
@@ -0,0 +1,26 @@
+const menu_btn = document.getElementById('menu-btn');
+const menu_items = document.getElementById('menu-items');
+
+// Menu button
+menu_btn.addEventListener('click', () => {
+    menu_items.classList.toggle('active');
+})
+
+// Cake slider
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: "#ftd--next-btn",
+        prevEl: "#ftd--prev-btn",
+    },
+    loop: true,
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        }
+      }
+});
--- script/menu.js
+++
@@ -1,6 +0,0 @@
-const menu_btn = document.getElementById('menu-btn');
-const menu_items = document.getElementById('menu-items');
-
-menu_btn.addEventListener('click', () => {
-    menu_items.classList.toggle('active');
-})
