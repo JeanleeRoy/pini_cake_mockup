d73cd58ea2034ffc0789dd5d0b1fdf116664f226
Jeanlee <jeanlee.barreto@utec.edu.pe>
2021-11-28 02:27:00 -0500

    Add responsive store options menu

--- docs/script/store.js
+++ docs/script/store.js
@@ -1,3 +1,6 @@
+const menuOption = document.getElementById('menuOption');
+const menuFilters = document.getElementById('storeFilters');
+const optionText = document.getElementById('storeOptionText');
 const filters = document.getElementsByClassName('filter-setting');
 const orderProds = document.getElementById('orderProds')
 
@@ -10,4 +13,27 @@ const toggleSubmenu = (submenuId) => {
     if (submenuId === 'orderProds')
         orderProds.classList.toggle('hide');
     else orderProds.classList.add('hide');
+}
+
+const showMobileOptions = (option) => {
+    document.body.classList.add('stop-scroll');
+    menuOption.style.display = 'flex';
+    if (option === 'filters') {
+        menuFilters.style.display = 'flex';
+        optionText.innerText = 'Filtrar';
+    }
+    if (option === 'order') {
+        orderProds.classList.remove('hide');
+        optionText.innerText = 'Ordenar';
+    }
+}
+
+const hideMobileOptions = () => {
+    document.body.classList.remove('stop-scroll');
+    menuOption.style.display = 'none';
+    console.log(optionText.innerText);
+    if (optionText.innerText === 'Filtrar')
+        menuFilters.style.display = 'none';
+    if (optionText.innerText === 'Ordenar')
+        orderProds.classList.add('hide');
 }
